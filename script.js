b76842ec0068ca71bf6a55642dc380f1ffa2eab0
DevByteTools <annainwork21@gmail.com>
2026-03-08 12:32:49 +0530

    Refactor age calculation and add new functions
    Updated calculateAge function to use the current year and modified result display. Added functions for calculating percentage and BMI.

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 function calculateAge(){
 
 let birthyear = document.getElementById("birthyear").value;
+let currentYear = new Date().getFullYear();
 
-let age = 2026 - birthyear;
+let age = currentYear - birthyear;
 
-document.getElementById("result").innerHTML =
-"Your age is " + age;
+document.getElementById("ageresult").innerHTML =
+"Your age is " + age + " years";
+
+}
+
+function calculatePercentage(){
+
+let percent = document.getElementById("percent").value;
+let number = document.getElementById("number").value;
+
+let result = (percent/100) * number;
+
+document.getElementById("percentresult").innerHTML =
+"Result: " + result;
+
+}
+
+function calculateBMI(){
+
+let weight = document.getElementById("weight").value;
+let height = document.getElementById("height").value;
+
+let bmi = weight / (height * height);
+
+document.getElementById("bmiresult").innerHTML =
+"Your BMI is " + bmi.toFixed(2);
 
 }
